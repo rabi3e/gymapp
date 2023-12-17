c9ed2355666f0e9963fcd832456f3c2cdf7c3bbc
chatouirabie <chatouirabie@gmail.com>
2023-12-17 22:00:58 +0100

    add abonement models

--- src/sport/models.py
+++ src/sport/models.py
@@ -14,7 +14,7 @@ class Adherent(models.Model):
     sexe = models.CharField(_("Sexe"), max_length=1, choices= SEX_TYPE)
     dte_naissance = models.DateField(_("Date de Naissance"), auto_now=False, auto_now_add=False)
     creation_dte = models.DateTimeField(_("Date d'enregistrement"),auto_now_add=True)
-    save_by = models.ForeignKey(User, verbose_name=_(""), on_delete=models.PROTECT)
+    save_by = models.ForeignKey(User, verbose_name=_(""), related_name='adherentuser' ,on_delete=models.PROTECT)
     image = models.ImageField(_("Image"), upload_to='Adherents',null=True,blank=True)
     adresse = models.CharField(_("Adresse"), max_length=700)
     ville = models.CharField(_("Ville"), max_length=100)
@@ -40,3 +40,17 @@ class Sport(models.Model):
         verbose_name = 'Sport'
         verbose_name_plural = 'Sports'
 
+class Abonement(models.Model):
+
+    adhrent = models.ForeignKey(Adherent, related_name='abonementadherent', on_delete=models.SET_NULL,blank=True, null=True)
+    sport = models.ForeignKey(Sport, related_name='abonementsport', on_delete=models.SET_NULL,blank=True, null=True)
+    debut = models.DateField(_("Date Debut"), auto_now=False, auto_now_add=False)
+    expiration = models.DateField(_("Expiration"), auto_now=False, auto_now_add=False)
+    prix = models.FloatField(_("Prix"))
+
+    def __str__(self):
+        return f"({self.adhrent} {self.sport} {self.expiration})"
+
+    class Meta:
+        verbose_name = 'Abonement'
+        verbose_name_plural = 'Abonements'
