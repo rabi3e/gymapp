7dd5046fcc673054c98802abe45a111558d4e314
chatouirabie <chatouirabie@gmail.com>
2023-12-27 22:45:18 +0100

    update adherent liste

--- src/sport/models.py
+++ src/sport/models.py
@@ -4,6 +4,17 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 
+class Sport(models.Model):
+    nom = models.CharField(_("Decipline"), max_length=50)
+    jour = models.CharField(_("Jour"), max_length=150)
+    prix = models.FloatField(_("Prix"))
+
+    def __str__(self):
+        return self.nom
+    class Meta:
+        verbose_name = 'Sport'
+        verbose_name_plural = 'Sports'
+
 class Adherent(models.Model):
     SEX_TYPE = [
         ('M', 'Masculin'),
@@ -15,7 +26,7 @@ class Adherent(models.Model):
     sexe = models.CharField(_("Sexe"), max_length=1, choices= SEX_TYPE)
     dte_naissance = models.DateField(_("Date de Naissance"), auto_now=False, auto_now_add=False)
     creation_dte = models.DateTimeField(_("Date d'enregistrement"),auto_now_add=True)
-    save_by = models.ForeignKey(User, verbose_name=_(""), related_name='adherentuser' ,on_delete=models.PROTECT)
+    save_by = models.ForeignKey(User, verbose_name=_("Createur"), related_name='adherentuser' ,on_delete=models.PROTECT)
     image = models.ImageField(_("Image"), upload_to='Adherents_image',null=True,blank=True)
     adresse = models.CharField(_("Adresse"), max_length=700)
     ville = models.CharField(_("Ville"), max_length=100)
@@ -23,6 +34,10 @@ class Adherent(models.Model):
     tel = models.CharField(_("Téléphone"), max_length=10)
     acte_naissance = models.ImageField(_("Acte de Naissance"), upload_to='Adherents_acte',null=True,blank=True)
     cin = models.ImageField(_("CIN"),upload_to='Adherents_cin',null=True,blank=True)
+    sport = models.ForeignKey(Sport,verbose_name=_("Sport"),on_delete=models.SET_NULL,null=True,blank=True)
+    payement = models.BooleanField(default=True)
+    
+    
     
     
     def __str__(self):
@@ -32,16 +47,7 @@ class Adherent(models.Model):
         verbose_name = 'Adherent'
         verbose_name_plural = 'Adherents'
 
-class Sport(models.Model):
-    nom = models.CharField(_("Decipline"), max_length=50)
-    jour = models.CharField(_("Jour"), max_length=150)
-    prix = models.FloatField(_("Prix"))
 
-    def __str__(self):
-        return self.nom
-    class Meta:
-        verbose_name = 'Sport'
-        verbose_name_plural = 'Sports'
 
 
 class Abonement(models.Model):
